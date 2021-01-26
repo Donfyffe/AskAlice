998ac8788ae343be78754a909bec863cd1a01f44
DFyffe <fyffed@netzero.net>
2021-01-25 20:53:59 -0500

    Brake turn speed red else green

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -100,7 +100,7 @@ typedef struct {
 
 
 typedef struct UIScene {
-
+  cereal::CarState::Reader car_state;
   mat4 extrinsic_matrix;      // Last row is 0 so we can use mat4.
   bool world_objects_visible;
 
