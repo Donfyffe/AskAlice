1bc4e31ac5d3a1b0e724bb02513c168e357db889
DFyffe <fyffed@netzero.net>
2021-01-31 08:57:27 -0500

    Revert " hyundai: update LFAHDA_MFC"
    This reverts commit c230afa7046b672ef0d7d4ed0705542190bd8579.

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -78,13 +78,24 @@ def create_clu11(packer, bus, clu11, button, speed, cnt):
   values["CF_Clu_AliveCnt1"] = cnt
   return packer.make_can_msg("CLU11", bus, values)
 
-def create_lfahda_mfc(packer, enabled, hda_set_speed=0):
+def create_lfa_mfa(packer, frame, enabled):
   values = {
-    "LFA_Icon_State": 2 if enabled else 0,
-    "HDA_Active": 1 if hda_set_speed else 0,
-    "HDA_Icon_State": 2 if hda_set_speed else 0,
-    "HDA_VSetReq": hda_set_speed,
+    "ACTIVE": enabled,
+    "HDA_USM": 2,
   }
+
+  # ACTIVE 1 = Green steering wheel icon
+
+  # LFA_USM 2 & 3 = LFA cancelled, fast loud beeping
+  # LFA_USM 0 & 1 = No mesage
+
+  # LFA_SysWarning 1 = "Switching to HDA", short beep
+  # LFA_SysWarning 2 = "Switching to Smart Cruise control", short beep
+  # LFA_SysWarning 3 =  LFA error
+
+  # ACTIVE2: nothing
+  # HDA_USM: nothing
+
   return packer.make_can_msg("LFAHDA_MFC", 0, values)
 
 def create_scc11(packer, enabled, set_speed, lead_visible, lead_dist, lead_vrel, lead_yrel, gapsetting, standstill,
